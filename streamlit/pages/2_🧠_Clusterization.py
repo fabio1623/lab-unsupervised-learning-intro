f2238761afd6340b3ce8fb7cebf005be2fd1f442
FabioSP <sarmentopedrofabio@gmail.com>
2023-02-11 00:12:28 +0100

    WIP

--- streamlit/pages/2_🧠_Clusterization.py
+++ streamlit/pages/2_🧠_Clusterization.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, SpectralClustering, AffinityPropagation
-from sklearn.mixture import GaussianMixture
+from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import davies_bouldin_score
+import pickle
 
 st.set_page_config(
     page_title="Model Generator",
@@ -25,46 +24,50 @@ def load_data(mongo_db_collection):
         data = clean_data(data)
         data = data.sample(frac=1)
         return data
+    
+def clusterize_data(dataframe):
+    with st.spinner('Clusterizing data...'):
+        X = dataframe.drop(['_id', 'name', 'artists'], axis=1)
+        y = dataframe['name']
+
+        model = KMeans(n_clusters=9)
+        min_max_scaler = MinMaxScaler().fit(X)
+        X_normalized = min_max_scaler.transform(X)
+        model.fit(X_normalized)
+
+        kmeans_model_with_scaler = {
+            "min_max_scaler": min_max_scaler,
+            "model": model
+        }
+
+        clusters = model.predict(X_normalized)
+        dataframe = pd.concat([dataframe,pd.Series(clusters, name='cluster')],axis=1)
+
+        return dataframe, kmeans_model_with_scaler
+    
+def save_data_and_model(dataframe, kmeans_model_with_scaler):
+    with st.spinner('Saving data into clusterized_data.csv...'):
+        dataframe.to_csv("data/clusterized_data.csv", index=False)
+    with st.spinner('Saving model and scaler into kmeans_model_with_scaler.pkl...'):
+        with open("data/kmeans_model_with_scaler.pkl", "wb") as file:
+            pickle.dump(kmeans_model_with_scaler, file)
+
 
 st.write("# 🧠 Model Tester")
 
-if "full_data" not in st.session_state:
-    st.session_state["full_data"] = None
+
 
 if "mongo_db_collection" not in st.session_state:
     st.session_state["mongo_db_collection"] = None
 
-if "kmeans_model_with_scaler" not in st.session_state:
-    st.session_state["kmeans_model_with_scaler"] = None
 
-if st.session_state["full_data"] is None:
-    if st.session_state["mongo_db_collection"] is None:
-        st.warning("Please, configure database connection in 'Database' section.")
-    else:
-        if st.button("Load data"):
-            st.session_state["full_data"] = load_data(st.session_state["mongo_db_collection"])
-            st.info("Database loaded.")
-            st.experimental_rerun()
+if st.session_state["mongo_db_collection"] is None:
+    st.warning("Please, configure database connection in 'Database' section.")
 else:
-    if st.button("Unload database"):
-        st.session_state["full_data"] = None
-        st.experimental_rerun()
-
-    X = st.session_state["full_data"].drop(['_id', 'name', 'artists'], axis=1)
-    y = st.session_state["full_data"]['name']
-
     if st.button("Clusterize data"):
-        with st.spinner('Clustering data...'):
-            model = KMeans(n_clusters=9)
-            min_max_scaler = MinMaxScaler().fit(X)
-            X_normalized = min_max_scaler.transform(X)
-            model.fit(X_normalized)
+        data = load_data(st.session_state["mongo_db_collection"])
+        data, kmeans_model_with_scaler = clusterize_data(data)
 
-            st.session_state["kmeans_model_with_scaler"] = {
-                "min_max_scaler": min_max_scaler,
-                "model": model
-            }
+        save_data_and_model(data, kmeans_model_with_scaler)
 
-            clusters = model.predict(X_normalized)
-            st.session_state["full_data"] = pd.concat([st.session_state["full_data"],pd.Series(clusters, name='cluster')],axis=1)
-            st.dataframe(st.session_state["full_data"][:10], use_container_width=True)
+        st.dataframe(data[:10], use_container_width=True)
--- streamlit/pages/3_Recommender.py
+++ streamlit/pages/3_Recommender.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import pickle
 
 def find_matches(dataframe, song_title):
     with st.spinner("Searching matches..."):
@@ -17,51 +19,111 @@ def get_recommendation(dataframe, matches, song_id):
         else:
             return random_song
 
+def load_data():
+    with st.spinner("Loading data..."):
+        try:
+            st.session_state["clusterized_data"] = pd.read_csv("data/clusterized_data.csv")
+        except:
+            pass
+
+def load_model_with_scaler():
+    with st.spinner("Loading model and scaler..."):
+        try:
+            with open(f'data/kmeans_model_with_scaler.pkl', "rb") as file:
+                st.session_state["kmeans_model_with_scaler"] = pickle.load(file)
+        except:
+            pass
+
+def init():
+    if "clusterized_data" not in st.session_state:
+        st.session_state["clusterized_data"] = None
+
+    if "kmeans_model_with_scaler" not in st.session_state:
+        st.session_state["kmeans_model_with_scaler"] = None
+
+    if "title" not in st.session_state:
+        st.session_state["title"] = None
+
+    if "matches" not in st.session_state:
+        st.session_state["matches"] = None
+
+    if "id" not in st.session_state:
+        st.session_state["id"] = None
+
+    if "recommended_song" not in st.session_state:
+        st.session_state["recommended_song"] = None
+
+
+def ask_title():
+    st.write("# Provide the title of a song you like")
+
+    st.session_state["title"] = st.text_input("Title")
+
+    if st.button("Search"):
+        matches = find_matches(st.session_state["clusterized_data"], st.session_state["title"])
+        if matches is None:
+            st.warning("No matching song found. Try another one.")
+        else:
+            st.session_state["matches"] = matches
+            st.experimental_rerun()
+
+def ask_id():
+    st.write("# Type _id of corresponding song")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.session_state["id"] = st.text_input("ID")
+
+        if st.button("Submit"):
+            recommended_song = get_recommendation(st.session_state["clusterized_data"], st.session_state["matches"], st.session_state["id"])
+            if recommended_song is None:
+                st.warning("No recommendation found.")
+            else:
+                st.session_state["recommended_song"] = recommended_song
+                st.experimental_rerun()
+
+        if st.button("Back to title selection"):
+            st.session_state["title"] = None
+            st.session_state["matches"] = None
+            st.experimental_rerun()
+
+    with col2:
+        display_matches()
+
+def display_matches():
+    nb_matches = st.session_state["matches"].shape[0]
+    title = st.session_state["title"]
+    st.write(f"### {nb_matches} matches for '{title}'")
+    st.dataframe(st.session_state["matches"][['_id', 'name', 'artists']], use_container_width=True)
+
+def display_recommendation():
+    st.dataframe(st.session_state["recommended_song"], use_container_width=True)
+
+    if st.button("Back to id selection"):
+        st.session_state["id"] = None
+        st.session_state["recommended_song"] = None
+        st.experimental_rerun()
+
 
 st.set_page_config(
     page_title="Recommender",
     page_icon="",
 )
 
-st.write("# Recommender")
+init()
 
-if "full_data" not in st.session_state:
-    st.session_state["full_data"] = None
+if st.session_state["clusterized_data"] is None or st.session_state["kmeans_model_with_scaler"] is None:
+    load_data()
+    load_model_with_scaler()
 
-if "kmeans_model_with_scaler" not in st.session_state:
-    st.session_state["kmeans_model_with_scaler"] = None
+if st.session_state["clusterized_data"] is None or st.session_state["kmeans_model_with_scaler"] is None:
+    st.error("clusterized_data.csv / kmeans_model_with_scaler.pkl could not be found. Please, run clusterize data in 'Clusterization' section.")
+    st.stop()
 
-if "matches" not in st.session_state:
-    st.session_state["matches"] = None
-
-if st.session_state["full_data"] is None or st.session_state["kmeans_model_with_scaler"] is None:
-    st.warning("Please, clusterize data in 'Clusterization' section.")
-elif st.session_state["matches"] is None:
-    with st.form("song-title-form"):
-        song_title = st.text_input("Provide the title of a song you like")
-
-        if st.form_submit_button("Search"):
-            matches = find_matches(st.session_state["full_data"], song_title)
-            if matches is None:
-                st.warning("No matching song found. Try another one.")
-            else:
-                st.session_state["matches"] = matches
-                st.experimental_rerun()
+if st.session_state["matches"] is None:
+    ask_title()
+elif st.session_state["id"] is None:
+    ask_id()
 else:
-    with st.form("song-id-form"):
-        if st.form_submit_button("Back"):
-            st.session_state["matches"] = None
-            st.experimental_rerun()
-            
-        song_id = st.text_input("Provide the id of the matching song")
-        recommended_song = None
-        if st.form_submit_button("Get recommendation"):
-            recommended_song = get_recommendation(st.session_state["full_data"], st.session_state["matches"], song_id)
-            if recommended_song is None:
-                st.warning("No recommendation found.")
-            else:
-                st.write("Recommended song")
-                st.dataframe(recommended_song[['_id', 'name', 'artists']], use_container_width=True)
-    
-    if recommended_song is None:
-        st.dataframe(st.session_state["matches"][['_id', 'name', 'artists']], use_container_width=True)
+    display_recommendation()
